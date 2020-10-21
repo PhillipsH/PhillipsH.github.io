64e7f6d4fbe452c9d7e1df2181d55d82d480e367
phillipsh <phillipshalim@gmaail.com>
2020-10-20 19:35:25 -0700

    website

---
+++ precache-manifest.5b119a9a8ce7e4d26b6d4c306090e115.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "946dc13453592fc202389e6d3731af47",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b42890fef4e5702070de",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "46cf0e0929f2122bf1c7",
+    "url": "./static/css/main.2a9635a1.chunk.css"
+  },
+  {
+    "revision": "b42890fef4e5702070de",
+    "url": "./static/js/2.710431a2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.710431a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46cf0e0929f2122bf1c7",
+    "url": "./static/js/main.aeb0a32e.chunk.js"
+  },
+  {
+    "revision": "4dbad56aa5f1eaec1e73",
+    "url": "./static/js/runtime-main.e534f6f2.js"
+  },
+  {
+    "revision": "6d5fb1c7996fe27820fe4947ec07b06d",
+    "url": "./static/media/CompostKing.6d5fb1c7.PNG"
+  },
+  {
+    "revision": "45dc8258b03d3c51a48914c6f8cead86",
+    "url": "./static/media/android-app.45dc8258.png"
+  },
+  {
+    "revision": "2c194427c551c3e628064e359ee8ad82",
+    "url": "./static/media/androidstudio.2c194427.png"
+  },
+  {
+    "revision": "6bb0fcd0d3f00077892b74ffebad764f",
+    "url": "./static/media/aws.6bb0fcd0.png"
+  },
+  {
+    "revision": "614e81bd92afc62be8127beac5e1b9af",
+    "url": "./static/media/cpp_logo.614e81bd.png"
+  },
+  {
+    "revision": "c26a73b45b9a801864f13edeb0aa2cfb",
+    "url": "./static/media/css3.c26a73b4.png"
+  },
+  {
+    "revision": "6a9950387dfa2a67ee0939f61d79e448",
+    "url": "./static/media/dialogflow.6a995038.png"
+  },
+  {
+    "revision": "2103c44598b4a6454d3df6e57b3d1415",
+    "url": "./static/media/docker.2103c445.png"
+  },
+  {
+    "revision": "eafbf66d78d8f234de76cea38c71ca2d",
+    "url": "./static/media/envelope.eafbf66d.svg"
+  },
+  {
+    "revision": "471a76f3b4f50c294870f0531c80e6b0",
+    "url": "./static/media/github.471a76f3.svg"
+  },
+  {
+    "revision": "f35778ad74c8f474363b19112259bcf9",
+    "url": "./static/media/homerbot.f35778ad.png"
+  },
+  {
+    "revision": "664a8c00ef526bf33edf55808601d461",
+    "url": "./static/media/html5.664a8c00.png"
+  },
+  {
+    "revision": "a7fd72b89f1b1bba4ec9b4200bdeb288",
+    "url": "./static/media/java.a7fd72b8.png"
+  },
+  {
+    "revision": "12bac607c472dfee8483d057e8515ec2",
+    "url": "./static/media/javascript.12bac607.png"
+  },
+  {
+    "revision": "aeba8b465575f550cccde5ed155f09d2",
+    "url": "./static/media/jenkins.aeba8b46.png"
+  },
+  {
+    "revision": "152d37b057a28c0e88319056448c7ef5",
+    "url": "./static/media/keebie.152d37b0.PNG"
+  },
+  {
+    "revision": "529d6e5ce85d0804bdc35d582656c07b",
+    "url": "./static/media/knowledge.529d6e5c.png"
+  },
+  {
+    "revision": "5ebb3469f907c3c887d6c1b9132c4eaa",
+    "url": "./static/media/linkedin.5ebb3469.svg"
+  },
+  {
+    "revision": "6dfc349801e573cb4a38c1d3e111c9ed",
+    "url": "./static/media/memory.6dfc3498.png"
+  },
+  {
+    "revision": "155b42c91749e8a3abe18b304edc0c78",
+    "url": "./static/media/mern.155b42c9.jpg"
+  },
+  {
+    "revision": "f503223cc1f45e9ce77ea2a91f9ba1e8",
+    "url": "./static/media/mysql.f503223c.svg"
+  },
+  {
+    "revision": "270f55fffea36219b66a63fbe03ff497",
+    "url": "./static/media/nodejs.270f55ff.png"
+  },
+  {
+    "revision": "8136f3a5001c7262cce2d4fc22296c7d",
+    "url": "./static/media/pipeline.8136f3a5.png"
+  },
+  {
+    "revision": "6c846f86c254dbcb27ff11f01e9821a3",
+    "url": "./static/media/portfolio.6c846f86.png"
+  },
+  {
+    "revision": "50156c71fb2cf47d5cfe134231975922",
+    "url": "./static/media/postgresql.50156c71.png"
+  },
+  {
+    "revision": "cc791bec145b9d9a77a047629fe32fd7",
+    "url": "./static/media/python.cc791bec.png"
+  },
+  {
+    "revision": "4912f6d800e1b75354e0b5d4794e1856",
+    "url": "./static/media/redux.4912f6d8.png"
+  },
+  {
+    "revision": "b52c746b927835ac0fcede110c5c9ee7",
+    "url": "./static/media/scrapy.b52c746b.png"
+  }
+]);
